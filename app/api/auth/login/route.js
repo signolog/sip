f6c8b9ff67e57f960166de121cfc0210379b843d
alinahmettekin <alinahmettekin@gmail.com>
2025-10-10 14:38:20 +0300

    fix: place content error

--- app/api/auth/login/route.js
+++ app/api/auth/login/route.js
@@ -57,7 +57,7 @@ export async function POST(request) {
       username: user.username,
       email: user.email,
       role: user.role,
-      place_id: user.place_id,
+      place_id: user.place_id?.toString(),
       store_id: user.store_id,
       last_login: user.last_login,
     };
